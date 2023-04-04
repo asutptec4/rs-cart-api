11d41b948aba3a3910409dc99db9663c1b765797
asutptec4 <a.shishonok@gmail.com>
2023-04-04 09:57:30 +0300

    feat: integrate order

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from '../database/database.module';
+import { cartProviders, orderProviders } from '../database/entities.providers';
+import { OrderController } from './order.controller';
 import { OrderService } from './services';
 
 @Module({
-  providers: [ OrderService ],
-  exports: [ OrderService ]
+  imports: [DatabaseModule],
+  providers: [...cartProviders, ...orderProviders, OrderService],
+  exports: [OrderService],
+  controllers: [OrderController],
 })
 export class OrderModule {}
